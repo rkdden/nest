ae426db51909235c5f5e517319e95bb7d61dff10
SY <12tndbs12@naver.com>
2021-06-18 22:42:42 +0900

    postgresql 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { User } from './users/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -5,7 +6,16 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "1234",
+      database: "shoppingmall",
+      entities: [User],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
